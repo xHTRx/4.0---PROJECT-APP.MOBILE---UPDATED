8d6c9b0df1234eba75d5fa791470ec9702379a7b
LINCOLN GABRIEL SOUSA MATOS <lincoln.matos@cs.up.edu.br>
2025-10-30 10:56:08 -0300

    Implementando firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     // 1. KSP - Mantido o mais recente
     id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+    // Firebase
+
 }
 
 android {
